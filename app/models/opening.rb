a0b8872ede24ab2f4dcf32b06abeb9bc8e9869f0
Thibault Hamel <contact@adaedra.eu>
2020-03-19 06:13:19 +0100

    Add basic validations on Opening

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Opening < ApplicationRecord
+  validates :title, presence: true
+  validates :company, presence: true
+  validates :description, presence: true
+
   scope :published, -> { where.not(published_at: nil).where('published_at <= current_timestamp') }
   scope :not_archived, -> { where(archived_at: nil).or(where('archived_at > current_timestamp')) }
   scope :active, -> { published.not_archived }
--- spec/models/opening_spec.rb
+++ spec/models/opening_spec.rb
@@ -15,4 +15,13 @@ RSpec.describe Opening, type: :model do
       expect(Opening.active).to eq([active])
     end
   end
+
+  it 'validates presence of required fields' do
+    record = Fabricate.build(:opening, title: nil, company: nil, description: nil)
+
+    expect(record.save).to be(false)
+    expect(record.errors.details[:title]).to include(error: :blank)
+    expect(record.errors.details[:company]).to include(error: :blank)
+    expect(record.errors.details[:description]).to include(error: :blank)
+  end
 end
